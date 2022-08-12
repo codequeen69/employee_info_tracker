621062875cdc26806984b6a99ef0db72d8486170
Venus DM <crhowe90@gmail.com>
2022-08-12 11:55:54 -0500

    database and tables schema created

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE department(
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     dept_name VARCHAR(30)
+     );
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,0),
+    department_id INTEGER,
+     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+     );
+
+CREATE TABLE employees (
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     first_name VARCHAR(30) NOT NULL,
+     last_name VARCHAR(30) NOT NULL,
+     role_id INTEGER,
+     manager_id INTEGER,
+     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE
+    );
--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const inquirer = require('inquirer');
+
+promptQuestion = () => {
+return inquirer.prompt([
+    {
+        type: 'list',
+        name: 'options',
+        message: 'Please choose one of the following options.',
+        choices:['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an Employee', 'Update an employee role']
+    }
+])
+};
+promptQuestion();
